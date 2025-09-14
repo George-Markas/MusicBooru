fea69c505ed4e0ba0518115659aac0e3642686ee
George-Markas <georgemarkanton@proton.me>
2025-09-15 00:51:21 +0300

    Implemented a basic (and somewhat problematic) login frontend
    Implemented a basic (and somewhat problematic) login frontend

--- src/main/java/com/example/musicbooru/MusicBooruApplication.java
+++ src/main/java/com/example/musicbooru/MusicBooruApplication.java
@@ -2,13 +2,6 @@ package com.example.musicbooru;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.lang.NonNull;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.http.HttpMethod;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class MusicBooruApplication {
