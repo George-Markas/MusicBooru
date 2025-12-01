cfef1d6510fae536f70db8462910fb3ad9d3127b
George-Markas <georgemarkanton@proton.me>
2025-12-01 23:39:24 +0200

    Removed unused import

--- src/main/java/com/example/musicbooru/util/Commons.java
+++ src/main/java/com/example/musicbooru/util/Commons.java
@@ -6,8 +6,4 @@ public class Commons {
     public final static String LIBRARY = "./library/";
     public final static String ARTWORK = "./artwork/";
     public final static String NO_COVER = "static/no_cover.jpg";
-
-    // TODO Support more audio formats
-    public final static String FILE_EXTENSION = ".flac";
-    public final static String MEDIA_TYPE_FLAC = "audio/flac"; // Not "audio/x-flac"
 }
