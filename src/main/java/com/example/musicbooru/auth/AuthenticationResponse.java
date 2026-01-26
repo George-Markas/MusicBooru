58e478cc9e2d6afc88af90328c11071e6e5f9859
aantdev <antonyantivachis@gmail.com>
2026-01-26 21:42:53 +0200

    Added JsonIgnore to cookieString of AuthenticationResponse DTO (Forgot to add it beforehand).

--- src/main/java/com/example/musicbooru/auth/AuthenticationResponse.java
+++ src/main/java/com/example/musicbooru/auth/AuthenticationResponse.java
@@ -1,9 +1,12 @@
 package com.example.musicbooru.auth;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.http.HttpStatusCode;
 
 public record AuthenticationResponse(
+        @JsonIgnore
         String cookieString,
+
         HttpStatusCode statusCode,
         String responseMessage
 ) {
